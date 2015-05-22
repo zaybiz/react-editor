995175ab21cc8137f797d3789d64b4414737006f
Henrique Alves <henriquea@gmail.com>
2015-05-22 10:40:38 +0100

    Remove socket.io

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-
-var io = require('socket.io')(http);
-var path = require('path');
-//var favicon = require('static-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 
 app.set('views', path.join(__dirname, 'app/templates'));
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(favicon());
-//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 
-
+// routing
 app.get('/', function(req, res) {
   res.render('index', { title: 'Main' });
 });
@@ -33,9 +24,6 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -57,14 +45,6 @@ app.use(function(err, req, res, next) {
 });
 
 app.set('port', process.env.PORT || 3000);
-
-io.on('connection', function(socket){
-  console.log('user connected ', socket.id);
-  socket.on('disconnect', function () {
-    console.log('User disconnected. %s. Socket id %s', socket.id);
-  });
-});
-
 
 http.listen(app.get('port'), function(){
   console.log('listen on: 3000');

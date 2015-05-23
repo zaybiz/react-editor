8163c0b2dff676340b8d4ed423f722241553eacc
Henrique Alves <henriquea@gmail.com>
2015-05-24 00:19:57 +0100

    Update default code

--- app/index.js
+++ app/index.js
@@ -3,12 +3,60 @@ import './styles/index.css';
 import React from 'react';
 import App from './components/App';
 
-const defaultCode = `var HelloMessage = React.createClass({
+const defaultCode = `// Code example from Facebook's tutorial
+// https://facebook.github.io/react/docs/tutorial.html
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var Comment = React.createClass({
   render: function() {
-    return <h3>Hello, {this.props.name}!</h3>;
+    return (
+      <div className="comment">
+        <h4 className="commentAuthor">
+          {this.props.author}
+        </h4>
+        {this.props.children}
+      </div>
+    );
   }
 });
-return <HelloMessage name="humans" />;
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h3>Comments</h3>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+return <CommentBox data={data} />;
 `;
 
 React.render(
